abb386f60f94e3217485ca108ff041b4356dbe37
Katie Palof <katie.palof@alaska.gov>
2018-02-13 15:59:16 -0900

    code to estimate density and biomass using current method

---
+++ code/biomass_density.R
@@ -0,0 +1,11 @@
+# code to explore alternative methods to estimate sea cucumber density in southeast alaska
+# this code will explore the current method (that uses Excel) and new methods that may fit the data better
+
+# K.Palof    ADF&G 
+# katie.palof@alaska.gov
+#2018-2-13
+
+# load ---
+source(./code/packages.R'')
+ 
+
