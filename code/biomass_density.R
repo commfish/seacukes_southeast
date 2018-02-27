21406be523f6c0b7ebd688ee970cb409d5230fcf
Katie Palof <katie.palof@alaska.gov>
2018-02-27 12:14:40 -0900

    reshape and clean data mean count per year

--- code/biomass_density.R
+++ code/biomass_density.R
@@ -10,7 +10,14 @@ source('./code/packages.R')
 #library(readxl)
  
 # data ---
-data <- read.csv('data/count_113_60s.csv')
+data <- read.csv('data/count_113_60s.csv', header = T, check.names = F) # added check.names = F to bring in year as column name without an X in front
 
 
 # clean and reshape data ---
+gather(data, "year", "n", 2:23) -> data2
+head(data2)
+
+# mean count ----
+data2 %>% 
+  group_by(year) %>% 
+  summarise(mean(n, na.rm = TRUE))
