c47600286ba3fc021f9284e0386d56bab3200602
Katie Palof <katie.palof@alaska.gov>
2018-02-28 16:47:34 -0900

    summary of density and weight (in pounds) with associated normal variances and sample sizes by year

--- code/biomass_density.R
+++ code/biomass_density.R
@@ -31,7 +31,8 @@ df %>%
   mutate(year = as.numeric(year), value = as.numeric(value)) -> wt_data2
 
 wt_data2 %>% 
-  filter(type == "avg") -> wt_data_avg
+  filter(type == "avg") %>% 
+  na.omit-> wt_data_avg
 
 # mean count ----
 data2 %>% 
@@ -45,14 +46,29 @@ data2 %>%
   mutate(se_d = sqrt(var_d/n_trans), sd_d = sqrt(var_d), 
          ps_se = (1- (1.31*(se_d)/mean_d))*100, ps_sd = (1- (1.31*(sd_d)/mean_d))*100 ) -> density_sum
 
-
-
-
 # mean weight -----
-# weight is in grams
+# weight is in grams and converted to pounds 
 wt_data_avg %>%     # data just with avg weights removed sample sizes for these weights
   group_by(year) %>% 
-  summarise(n_trans = n(), mean_wt = mean(value, na.rm = TRUE), 
-            var_wt = var(value, na.rm = TRUE)/n_trans) %>% 
-  mutate(se_wt = sqrt(var_wt/n_trans), sd_wt = sqrt(var_wt), 
+  summarise(n_trans_wt = n(), mean_wt = mean(value, na.rm = TRUE)/454, 
+            var_wt = var(value, na.rm = TRUE)/n_trans_wt/454^2) %>% 
+  mutate(se_wt = sqrt(var_wt/n_trans_wt), sd_wt = sqrt(var_wt), 
          ps_se = (1- (1.38*(se_wt)/mean_wt))*100, ps_sd = (1- (1.38*(sd_wt)/mean_wt))*100 ) -> weight_sum
+
+
+# density and weight -----
+density_sum %>% 
+  mutate(year = as.numeric(year)) %>% 
+  select(year, n_trans, mean_d, var_d) -> temp1
+
+weight_sum %>% 
+  select(year, n_trans_wt, mean_wt, var_wt) -> temp2
+
+temp1 %>% 
+  left_join(temp2) -> all_summary
+# log normal variance -----
+
+# 
+
+
+
