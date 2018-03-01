d5e6cf4526f89443df510ac0ae47078e89311451
Katie Palof <katie.palof@alaska.gov>
2018-02-28 16:02:59 -0900

    weight data import and clean up

--- code/biomass_density.R
+++ code/biomass_density.R
@@ -11,12 +11,27 @@ source('./code/packages.R')
  
 # data ---
 data <- read.csv('data/count_113_60s.csv', header = T, check.names = F) # added check.names = F to bring in year as column name without an X in front
-weights <- read.csv('data/wt_113_60s.csv', header = T, check.names = F)
+
+# weight data ---
+wt_data <- read.csv('data/wt_113_60s.csv', header = F, check.names = F)
 
 # clean and reshape data ---
 gather(data, "year", "n", 2:23) -> data2
 data2 %>% na.omit -> data2 # remove rows with no data for simplification in analyses
 head(data2)
+
+# reshape data to have row for avg weight and n - sample size 
+col.names <- c("transect", tail(paste(t(wt_data[1,]),t(wt_data[2,]),sep="."),n=20))
+df <- wt_data[-(1:2),]
+df <- setNames(df, col.names)
+df %>% 
+  gather(year, value, -transect) %>% 
+  mutate(year = as.character(year)) %>% 
+  separate(year, c("year", "type")) %>% 
+  mutate(year = as.numeric(year), value = as.numeric(value)) -> wt_data2
+
+wt_data2 %>% 
+  filter(type == "avg") -> wt_data_avg
 
 # mean count ----
 data2 %>% 
@@ -31,3 +46,13 @@ data2 %>%
          ps_se = (1- (1.31*(se_d)/mean_d))*100, ps_sd = (1- (1.31*(sd_d)/mean_d))*100 ) -> density_sum
 
 
+
+
+# mean weight -----
+# weight is in grams
+wt_data_avg %>%     # data just with avg weights removed sample sizes for these weights
+  group_by(year) %>% 
+  summarise(n_trans = n(), mean_wt = mean(value, na.rm = TRUE), 
+            var_wt = var(value, na.rm = TRUE)/n_trans) %>% 
+  mutate(se_wt = sqrt(var_wt/n_trans), sd_wt = sqrt(var_wt), 
+         ps_se = (1- (1.38*(se_wt)/mean_wt))*100, ps_sd = (1- (1.38*(sd_wt)/mean_wt))*100 ) -> weight_sum
