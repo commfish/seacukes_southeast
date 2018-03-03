0befe3781271d0710709717bd98b8146925f686f
Katie Palof <katie.palof@alaska.gov>
2018-03-02 16:27:08 -0900

    same as previous

--- code/biomass_density.R
+++ code/biomass_density.R
@@ -89,7 +89,8 @@ biomass_calc %>%  # multiple ways to get one sided 90% confidence intervals
          P_log_sd = (1-((lb_m - l90_log_sd)/lb_m))) -> c_inter_all# one-sided 90% large sample size about 1.28
 write.csv(c_inter_all, './results/confidence_intervals_current_113_60.csv')
 
-# 
+# distributino of count data by year -----
+
 
 
 
